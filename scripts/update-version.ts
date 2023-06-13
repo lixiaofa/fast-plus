fbca0e8b16435d1bd7753ebc6134b7abb98bc3b9
lixiaofa <li_xf@hisuntech.com>
2023-06-13 10:12:43 +0800

    ci(ci): warning  Unexpected console statement  no-console

--- scripts/update-version.ts
+++ scripts/update-version.ts
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: 司马老贼
  * @Date: 2023-05-31 12:07:17
- * @LastEditTime: 2023-06-11 21:46:35
+ * @LastEditTime: 2023-06-13 10:01:22
  * @LastEditors: 司马老贼
  */
 import consola from 'consola'
@@ -14,8 +14,6 @@ async function main() {
   const tagVersion = process.env.TAG_VERSION
 
   const gitHead = process.env.GIT_HEAD
-  console.log('process.env.TAG_VERSION', process.env)
-  console.log('process.env.GIT_HEAD', process.env.GIT_HEAD)
 
   if (!tagVersion || !gitHead) {
     errorAndExit(
