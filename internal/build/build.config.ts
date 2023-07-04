7d6198689a6eeeb5f915552bdfe4ffa1b7b485d0
lixiaofa <li_xf@hisuntech.com>
2023-07-04 22:25:38 +0800

    ci(ci): ReferenceError: __name is not defined

--- internal/build/build.config.ts
+++ internal/build/build.config.ts
@@ -1,5 +1,9 @@
 import { defineBuildConfig } from 'unbuild'
+const __defProp = Object.defineProperty
+const __name = (target, value) =>
+  __defProp(target, 'name', { value, configurable: true })
 
+globalThis.__name = __name
 export default defineBuildConfig({
   entries: ['src/index'],
   clean: true,
