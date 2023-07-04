6e139ba998d9f8ed0e99eb53f26fe17776e176f2
lixiaofa <li_xf@hisuntech.com>
2023-07-04 10:15:58 +0800

    ci(ci): ReferenceError: __name is not defined

--- internal/build/src/tasks/modules.ts
+++ internal/build/src/tasks/modules.ts
@@ -53,7 +53,6 @@ export const buildModules = async () => {
         loaders: {
           '.vue': 'ts',
         },
-        keepNames: false,
       }),
     ],
     external: await generateExternal({ full: false }),
